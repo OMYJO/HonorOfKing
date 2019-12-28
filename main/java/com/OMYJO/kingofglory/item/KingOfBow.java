7558ae78c1659e29a1227c5ebbd9ecdc3c8535df
MaYingjian <742862395@qq.com>
2019-12-28 17:07:12 +0800

    RT

--- main/java/com/OMYJO/kingofglory/item/KingOfBow.java
+++ main/java/com/OMYJO/kingofglory/item/KingOfBow.java
@@ -1,11 +1,14 @@
 package com.OMYJO.kingofglory.item;
 
+import com.OMYJO.kingofglory.other.KingOfMaterial;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
 
 public class KingOfBow extends BowItem implements KingOfItem
 {
-	public KingOfBow(Properties builder)
+	public KingOfBow(Rarity rarity)
 	{
-		super(builder);
+		super((new Item.Properties()).maxDamage(new KingOfMaterial().getMaxUses()).rarity(rarity).group());
 	}
 }
